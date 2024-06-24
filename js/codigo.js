e59daec9187e9cee0f7ae66948a2aaa5e3d73031
Lauluci <olivetioliveti@gmail.com>
2024-06-24 23:53:20 +0200

    1
    3

--- js/codigo.js
+++ js/codigo.js
@@ -1,79 +1,45 @@
 console.log("FYLL");
 
 // Tomo el archivo JSON y lo devuelvo por pantalla
-fetch("json/articulos.json").then(function (e) {
-    return e.json();
-}).then(function (data) {
-    console.log(data);
-    console.log("ahora voy a tomar los datos y los voy a poner en la web");
-    var numerodearticulos = data.articulos.length;
-    console.log("En realidad tengo " + numerodearticulos + " articulos en el archivo JSON");
-
-    // Tomo la URL del navegador
-    const miurl = window.location.href;
-    // Parto la URL en dos trozos, tomando el signo igual como separador
-    partido = miurl.split("=");
-
-    // En el caso de que el segundo elemento no exista
-    if (partido[1] == "Tienda") {
-
-        // Pintame TODOS los artículos
-        for (var i = 0; i < numerodearticulos; i++) {
-            document.getElementById("articulos").innerHTML += '<a href="?id=' + i + '"><article><div class="imagen" style="background:url(\'imagenes/' + data.articulos[i].imagen + '\');background-size:cover;background-position:center center;"></div><h4>' + data.articulos[i].titulo + '</h4><p>' + data.articulos[i].texto + '</p></article></a>';
+fetch("json/articulos.json")
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
-    }
-    console.log(partido[0].split("?")[1])
-    // Si no tiene el interrogante
-    // O detras del interrogante no hay nada
-    if(
-        window.location.href.indexOf("?") == -1
-        ||
-        partido[0].split("?")[1] == ""
-    ){
-        console.log("debes estar en la pagina de inicio")
-        fetch('paginas/Inicio.html').then(function (response) {
-            return response.text();
-        }).then(function (html) {
-            // Carga un convertidor de texto a HTML
-            var parser = new DOMParser();
-            // Convierte el contenido en HTML
-            var doc = parser.parseFromString(html, 'text/html');
-            // Lanzalo por pantalla
-            console.log(doc)
-            document.getElementById("articulos").innerHTML += doc.documentElement.innerHTML
-        })
-    }else{
-        document.getElementById("articulos").style.paddingTop = 0
-    }
-    if (partido[0].split("?")[1] == "p") {
-        // Carga una pagina HTML
-        fetch('paginas/'+partido[1]+'.html').then(function (response) {
-            return response.text();
-        }).then(function (html) {
-            // Carga un convertidor de texto a HTML
-            var parser = new DOMParser();
-            // Convierte el contenido en HTML
-            var doc = parser.parseFromString(html, 'text/html');
-            // Lanzalo por pantalla
-            console.log(doc);
-            document.getElementById("articulos").innerHTML += doc.documentElement.innerHTML;
-        });
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+        console.log("Ahora voy a tomar los datos y los voy a poner en la web");
+        var numerodearticulos = data.articulos.length;
+        console.log("En realidad tengo " + numerodearticulos + " artículos en el archivo JSON");
 
-    }
-    if (partido[1] != null) {
+        // Tomo la URL del navegador
+        const miurl = window.location.href;
+        // Parto la URL en dos trozos, tomando el signo igual como separador
+        let partido = miurl.split("=");
+        let pagina = partido[1] ? partido[1] : "Home";
 
-        // Y en el caso de que SI que exista el signo igual
-        // En ese caso pintame/(muestrame  el articulo que yo quiero) SOLO el articulo cuyo ID sea el id que hay en la url
-        document.getElementById("video").remove();
-        document.getElementById("articulos").innerHTML += '<article><div class="imagen" style="background:url(\'imagenes/' + data.articulos[partido[1]].imagen + '\');background-size:cover;background-position:center center;"></div><h4>' + data.articulos[partido[1]].titulo + '</h4><p>' + data.articulos[partido[1]].texto + '</p>';
-
-        if (data.articulos[partido[1]].codigovideo != null) {
-            document.getElementById("articulos").innerHTML += `<iframe  src="https://www.youtube.com/embed/${data.articulos[partido[1]].codigovideo}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        // Lógica para mostrar los artículos según la página actual
+        if (pagina === "Hoddies") {
+            // Mostrar solo los artículos de la categoría "Hoodies"
+            document.getElementById("articulos").innerHTML = ''; // Limpiar el contenido actual
+            for (var i = 0; i < numerodearticulos; i++) {
+                if (data.articulos[i].categoria === "Hoodies") {
+                    document.getElementById("articulos").innerHTML += `
+                        <article>
+                            <div class="imagen" style="background:url('imagenes/${data.articulos[i].imagen}'); background-size:cover; background-position:center center;"></div>
+                            <h4>${data.articulos[i].titulo}</h4>
+                            <p>${data.articulos[i].texto}</p>
+                        </article>`;
+                }
+            }
+        } else {
+            // Cargar contenido por defecto o manejar otras páginas según sea necesario
+            console.log("Cargando contenido por defecto o manejo de otras páginas...");
         }
 
-        document.getElementById("articulos").innerHTML += '</article>';
-        document.getElementById("articulos").style.paddingTop = "0px";
-        document.getElementsByTagName("article")[0].style.width = "100%";
-    }
-
-});
+    })
+    .catch(function(error) {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
